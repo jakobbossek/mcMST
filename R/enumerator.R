5511221d9e95e96fc1022b8f12982389974e7662
Jakob Bossek <info@jakobbossek.de>
2017-07-05 11:43:00 +0200

    docs for enumerators

--- R/enumerator.R
+++ R/enumerator.R
@@ -1,6 +1,19 @@
+#' @title Enumerate all solution candidates
+#'
+#' @description These functions enumerate all candidate solutions for
+#' a certain combinatorial optimization problem, e.g., all permutations
+#' for a TSP or all Pruefer-codes for a MST problem.
+#'
+#' @param n [\code{integer(1)}]\cr
+#'   Instance size.
+#' @return [\code{matrix}] Each row contains a candidate solution.
+#' @example
+#' sols = enumerateTSP(4L)
+#' @rdname enumerators
+#' @export
 enumerateTSP = function(n) {
   n = asInt(n, lower = 3L)
-  requirePackages("gtools", why = "enumerateTSP")
+  requirePackages("gtools", why = "rmoco::enumerateTSP")
   perms = gtools::permutations(n - 1L, n - 1L, v = 2:n)
   # drop duplicated permutations (symmetric TSP)
   perms = perms[1:(nrow(perms) / 2), , drop = FALSE]
@@ -8,8 +21,10 @@ enumerateTSP = function(n) {
   return(perms)
 }
 
+#' @rdname enumerators
+#' @export
 enumerateMCMST = function(n) {
   n = asInt(n, lower = 3L)
-  requirePackages("gtools", why = "enumerateMCMST")
+  requirePackages("gtools", why = "rmoco::enumerateMCMST")
   gtools::permutations(n, n - 2L, seq_len(n), repeats.allowed = TRUE)
 }
